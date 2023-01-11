174040c5302d8297e7ea902c69e1aaf9b5c37d0e
nickchan23 <nick.chan@utexas.edu>
2023-01-10 23:14:06 -0600

    Pathrise

---
+++ Python/Pathrise.py
@@ -0,0 +1,24 @@
+# Pathrise Software Engineering
+
+# Find the kth largest element in an unsorted array. 
+# Note that it is the kth largest element in the sorted order, 
+# not the kth distinct element. 
+
+# What is a second way you might do the same problem? 
+# Which approach would you prefer and why?
+
+# sort the array, access the kth element
+# k=1, returns largest element
+def find_largest1(self, arr: list, k: int) -> int:
+    temp = arr.sort()
+    return temp[-k]
+    # O(NlogN)
+
+def find_largest2(self, arr: list, k: int) -> int:
+    temp = arr
+    for i in range(0, k-1):
+        temp.remove(max(temp))
+    return max(temp)
+    # O(N)*k + O(N) = O(N)
+
+# The first approach is better as it has a better time complexity
